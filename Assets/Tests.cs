97edddd4869dde7108065da1adef6960dd033eff
Olle Kaiser <yogsther@gmail.com>
2021-04-15 10:35:44 +0200

    Added description to test card spawner

--- Assets/Tests.cs
+++ Assets/Tests.cs
@@ -26,6 +26,7 @@ public class Tests : MonoBehaviour
                 Transform testCard = Instantiate(cardPrefab, canvas.transform).transform;
                 testCard.GetComponent<RectTransform>().localPosition = new Vector3(-342.2f + (x * 50), 103.4f - (y * 130));
                 testCard.transform.Find("Name").GetComponent<Text>().text = card.name;
+                testCard.transform.Find("Description").GetComponent<Text>().text = card.description;
                 testCard.transform.Find("Image").GetComponent<Image>().sprite = card.image;
             }
         };
